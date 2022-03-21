b21370ad6668309b43bbb1807d0b329047596190
Shivam <shivam.kumar@jellyfishtechnologies.com>
2022-03-21 18:44:43 +0530

    Database

--- quiz/settings.py
+++ quiz/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'quiz',
-#         'USER': 'shivam',
-#         'PASSWORD': 'Shivam@16',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quiz',
+        'USER': 'shivam',
+        'PASSWORD': 'Shivam@16',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
